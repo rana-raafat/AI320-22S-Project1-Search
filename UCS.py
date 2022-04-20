84a1390d059e3b0c69c5da078652d82b1296af6c
Yosr Gamal <yosr.elqott@gmail.com>
2022-04-20 03:06:07 +0200

    UCS function added

---
+++ UCS.py
@@ -0,0 +1,40 @@
+def ucs(graph, start, goal):
+
+    # n is the key and the value is (inf)
+    # to set the cost of each cell by setting the value of each key(cell) to values from 1 to infinity
+    queue = {n: float('inf') for n in graph.grid}
+    queue[start] = 0  # we make the start cell's cost = 0
+    visited = {}
+    ucsPath = {}
+    while queue:
+        # We get the cell with the minimum cost
+        currCell = min(queue, key=queue.get)
+        visited[currCell] = queue[currCell]
+        if currCell == goal:
+            break
+        for d in 'ESNW':
+            if graph.maze_map[currCell][d] == True:
+                if d == 'E':
+                    neighbour = (currCell[0], currCell[1]+1)
+                elif d == 'W':
+                    neighbour = (currCell[0], currCell[1]-1)
+                elif d == 'S':
+                    neighbour = (currCell[0]+1, currCell[1])
+                elif d == 'N':
+                    neighbour = (currCell[0]-1, currCell[1])
+                if neighbour in visited:
+                    continue
+                cost = queue[currCell]+1  # we calculate the new cost
+
+                if cost < queue[neighbour]:
+                    queue[neighbour] = cost
+                    ucsPath[neighbour] = currCell
+        queue.pop(currCell)
+
+    revPath = {}
+    cell = goal
+    while cell != start:
+        revPath[ucsPath[cell]] = cell
+        cell = ucsPath[cell]
+
+    return revPath, visited[goal]  # return the path and the total cost
